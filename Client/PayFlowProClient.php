792675c4af8fe117da691e971d14ec5e29ba0655
Jeremy Bond <jebond@trollandtoad.com>
2018-07-17 16:16:06 -0400

    woot

--- Client/PayFlowProClient.php
+++ Client/PayFlowProClient.php
@@ -1,8 +1,8 @@
 <?php
 /**Composed by J.Bond 7/17/2018 This is the PayFlowPro communication class that will generate a client
-and handle communication between the PaymentProvider Class and PayFlowPro.
-This client will be utilized by the PaymentProvider class to handle transactions
-from checkout**/
+*and handle communication between the PaymentProvider Class and PayFlowPro.
+*This client will be utilized by the PaymentProvider class to handle transactions
+from checkout*/
 
 
 namespace Client {
