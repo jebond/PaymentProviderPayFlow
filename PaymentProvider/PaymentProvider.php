cec9dfa49ec614a5bb891980d7080ef8a0721129
Jeremy Bond <jebond@trollandtoad.com>
2018-07-17 14:52:58 -0400

    Initial Commit

---
+++ PaymentProvider/PaymentProvider.php
@@ -0,0 +1,16 @@
+<?php
+/**Composed by J.Bond 7/17/2018 This is a base class that will encapsulate any credit card processing endpoint that
+the checkout will use.
+Attempting to abstract enough so that we can keep checkout a separate entity to allow for splitting it off into
+a microservice at some point. Additionally, I am creating the classes for Payflow Pro communications via a client
+ that will be a part of this PaymentProvider**/
+
+namespace PaymentProvider {
+
+    
+
+    class PaymentProvider
+    {
+
+    }
+}
